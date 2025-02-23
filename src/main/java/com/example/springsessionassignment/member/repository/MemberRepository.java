84e466922c5db4ce5d663b417921a2af685614ee
younghwan314 <younghwan314@naver.com>
2025-02-24 08:48:00 +0900

    member 파트 수정

--- src/main/java/com/example/springsessionassignment/member/repository/MemberRepository.java
+++ src/main/java/com/example/springsessionassignment/member/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package com.example.springsessionassignment.member.repository;
 import com.example.springsessionassignment.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
 }
