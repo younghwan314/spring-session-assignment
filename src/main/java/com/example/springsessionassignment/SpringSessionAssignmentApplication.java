215752fb1dcc53949933ca3cd48b969902aac98a
younghwan314 <younghwan314@naver.com>
2025-02-24 08:12:22 +0900

    git init

---
+++ src/main/java/com/example/springsessionassignment/SpringSessionAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsessionassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessionAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSessionAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsessionassignment/SpringSessionAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsessionassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSessionAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
